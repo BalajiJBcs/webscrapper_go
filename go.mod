7a89e59d6d6b19cf7753c3634ddac6fffa420409
BalajiJBcs <jbbalajics@gmail.com>
2020-08-01 10:17:28 +0530

    main: bring up the crawler page
    Signed-off-by: BalajiJBcs <jbbalajics@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module webscrapper_in_go
 go 1.14
 
 require (
-	github.com/gofiber/fiber v1.13.3 // indirect
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/gofiber/fiber v1.13.3
 	go.mongodb.org/mongo-driver v1.4.0 // indirect
 )
--- server.go
+++ server.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gofiber/fiber"
 	"log"
+	"net/http"
 )
 
 const port = 8000
 
-
 func getProduct(c *fiber.Ctx) {
-	log.Println(c.Params("id"))
 	var product Product
 	json.Unmarshal([]byte(c.Body()), &product)
-	log.Println(product.WebUrl)
-	log.Println(product.Type)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", product.WebUrl, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Set("User-Agent", "Golang_Spider_Bot/3.0")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	log.Println(doc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(doc.Find("meta[name='title']").Attr("content"))
+	log.Println(doc.Find("meta[name='description']").Attr("content"))
+	log.Println(doc.Find("#priceblock_dealprice").Text())
+	log.Println(doc.Find("#priceblock_ourprice").Text())
+	log.Println(doc.Find("#acrCustomerReviewText").Text())
+	log.Println(doc.Find("#landingImage").Attr("data-old-hires"))
 }
 
 func main() {
- log.Println("App Logged")
 	app := fiber.New()
 	app.Post("/product/", getProduct)
 	app.Listen(port)
